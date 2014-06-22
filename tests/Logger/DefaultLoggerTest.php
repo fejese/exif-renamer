302e22561307c8297a31ab308acbe25c6bb1c1a9
Endre Fejes <fejese@fejese.com>
2014-06-23 00:17:11 +0200

    Adding tests for defaultlogger::log

---
+++ tests/Logger/DefaultLoggerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace fejese\ExifRenamer\Tests\Logger;
+
+use fejese\ExifRenamer\Logger\DefaultLogger;
+
+class DefaultLoggerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var resource
+     */
+    protected $tmpFile = null;
+
+    /**
+     * @var DefaultLogger
+     */
+    protected $logger;
+
+    public function setUp()
+    {
+        $this->tmpFile = tempnam(sys_get_temp_dir(), 'log');
+        $this->logger = new DefaultLogger($this->tmpFile);
+    }
+
+    /**
+     * @dataProvider messageProvider
+     */
+    public function testLogWritesToFileNameGiven(array $messages)
+    {
+        ob_start();
+        foreach ($messages as $message) {
+            $this->logger->log($message);
+        }
+        ob_end_clean();
+        $this->assertEquals(
+            implode('', $messages),
+            file_get_contents($this->tmpFile)
+        );
+    }
+
+    /**
+     * @dataProvider messageProvider
+     */
+    public function testLogWritesToStandardOut(array $messages)
+    {
+        ob_start();
+        foreach ($messages as $message) {
+            $this->logger->log($message);
+        }
+        $output = ob_get_clean();
+        $this->assertEquals(
+            implode('', $messages),
+            $output
+        );
+    }
+
+    public function tearDown()
+    {
+        if ($this->tmpFile) {
+            unlink($this->tmpFile);
+        }
+    }
+
+    public function messageProvider()
+    {
+        $messages = array(
+            array(array('Test message')),
+            array(array('Message with trailing space ')),
+            array(array("Message with new line\n")),
+            array(array("Multi \n line\nmessage")),
+            array(
+                array(
+                    'Multi part ',
+                    'message example',
+                    "with new line\n",
+                    'and trailing space '
+                )
+            )
+        );
+
+        return $messages;
+    }
+}
+
+
