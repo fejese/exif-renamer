49215b88853d73ca66d4a3b79cc9beff98ce9794
Endre Fejes <fejese@fejese.com>
2014-06-22 16:41:30 +0200

    Adding logger related classes

--- rename.php
+++ rename.php
@@ -3,6 +3,110 @@
 
 namespace fejese\ExifRename;
 
+class LoggerException extends \Exception
+{
+}
+
+interface LoggerInterface
+{
+    /**
+     * Logs a message
+     *
+     * @param string $message
+     * @throws LoggerException
+     * @return self
+     */
+    public function log($message);
+
+    /**
+     * Logs a line of message and appends a new line
+     *
+     * @param string $line
+     * @throws LoggerException
+     * @return self
+     */
+    public function logLine($line);
+}
+
+class DefaultLogger implements LoggerInterface
+{
+    /**
+     * @var string
+     */
+    private $logfileName;
+
+    /**
+     * @var resource
+     */
+    private $fileHandler = null;
+
+    /**
+     * Contructor setting the file name
+     *
+     * @param string $fileName
+     */
+    public function __construct($fileName)
+    {
+        $this->logfileName = $fileName;
+    }
+
+    /**
+     * Retrieves the file handler
+     *
+     * @throws \Exception if file is not writable
+     * @return resource
+     */
+    private function getFileHandler()
+    {
+        if (is_null($this->fileHandler)) {
+            $handler = fopen($this->logfileName, 'w');
+            if (!$handler) {
+                throw new LoggerException(sprintf('Failed to open log file for writing', $this->logfileName));
+            }
+            $this->fileHandler = $handler;
+        }
+
+        return $this->fileHandler;
+    }
+
+    /**
+     * @see LoggerInterface::log()
+     */
+    public function log($message)
+    {
+        $fileHandler = null;
+        try {
+            $fileHandler = $this->getFileHandler();
+        } catch (LoggerException $le) {
+            throw $le; // Bubble up
+        }
+
+        fwrite($fileHandler, $message);
+
+        return $this;
+    }
+
+    /**
+     * @see LoggerInterface::logLine()
+     */
+    public function logLine($line)
+    {
+        try {
+            return $this->log($line . "\n");
+        } catch (LoggerException $le) {
+            throw $le; // Bubble up
+        }
+    }
+
+    public function __destruct()
+    {
+        if (!is_null($this->fileHandler)) {
+            fclose($this->fileHandler);
+            $this->fileHandler = null;
+        }
+    }
+}
+
 class Renamer
 {
     private $logfileBaseName = 'rename.log';
