af736f5087666901a7325fd16549ababb398f21a
Endre Fejes <fejese@fejese.com>
2014-07-02 23:16:58 +0200

    Adding tests for zero logger

---
+++ tests/Logger/ZeroLoggerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace fejese\ExifRenamer\Tests\Logger;
+
+use fejese\ExifRenamer\Logger\ZeroLogger;
+
+class ZeroLoggerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var resource
+     */
+    protected $tmpFile = null;
+
+    /**
+     * @var ZeroLogger
+     */
+    protected $logger;
+
+    public function setUp()
+    {
+        $this->logger = new ZeroLogger();
+    }
+
+    /**
+     * @dataProvider fejese\ExifRenamer\Tests\DataProvider\Message::getMessageLists
+     */
+    public function testLogDoesNotGenerateOutput(array $messages)
+    {
+        ob_start();
+        foreach ($messages as $message) {
+            $this->logger->log($message);
+        }
+        $output = ob_get_clean();
+        $this->assertEquals(0, strlen($output));
+    }
+
+    /**
+     * @dataProvider fejese\ExifRenamer\Tests\DataProvider\Message::getMessageLists
+     */
+    public function testLogLineDoesNotGenerateOutput(array $messages)
+    {
+        ob_start();
+        foreach ($messages as $message) {
+            $this->logger->logLine($message);
+        }
+        $output = ob_get_clean();
+        $this->assertEquals(0, strlen($output));
+    }
+}
+
+
