b3f3d3af7e30c91b081f275559be6acf64365a3c
Endre Fejes <fejese@fejese.com>
2014-07-05 23:47:43 +0200

    Adding tests for modification date based renaming

--- tests/src/Modules/RenamerTest.php
+++ tests/src/Modules/RenamerTest.php
@@ -3,14 +3,26 @@
 namespace fejese\ExifRenamer\Tests\Modules;
 
 use fejese\ExifRenamer\Renamer;
+use fejese\ExifRenamer\Tests\Helpers\TestFileManager;
 
 class RenamerTest extends \PHPUnit_Framework_TestCase
 {
     private $renamer;
 
+    private $testFilesSrc;
+    private $testFilesTmp;
+
     public function setUp()
     {
-        $this->renamer = new Renamer('.');
+        $this->testFileSrc =  __DIR__ . '/../../files';
+        $this->testFilesTmp =  __DIR__ . '/../../tmp';
+        $this->testFileManager = new TestFileManager(
+            $this->testFileSrc,
+            $this->testFilesTmp
+        );
+        $this->testFileManager->copyToTemp();
+
+        $this->renamer = new Renamer($this->testFilesTmp);
     }
 
     /**
@@ -23,6 +35,32 @@ class RenamerTest extends \PHPUnit_Framework_TestCase
             $format,
             $this->renamer->getFormat()
         );
+    }
+
+    /**
+     */
+    public function testImagesWithoutExifRenamedToMTime()
+    {
+        $file = $this->testFilesTmp . '/noexif.png';
+        $mtime = filemtime($file);
+        $md5 = md5_file($file);
+
+        $newFile = sprintf(
+            '%s/%d_M.png',
+            $this->testFilesTmp,
+            $mtime
+        );
+
+        $this->renamer->setFormat('U');
+        $this->renamer->rename();
+
+        $this->assertTrue(file_exists($newFile));
+        $this->assertEquals($md5, md5_file($newFile));
+    }
+
+    public function tearDown()
+    {
+        $this->testFileManager->cleanupTemp();
     }
 }
 
